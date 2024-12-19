2788cc19e145c93da8d4029c640dcc03350b7505
Arif OKCAN <142164597+w7h3r@users.noreply.github.com>
2024-12-20 00:12:40 +0300

    Update general.lua

--- lua/settings/general.lua
+++ lua/settings/general.lua
@@ -5,6 +5,5 @@ vim.o.hlsearch = false               -- Disable search highlight
 vim.o.hidden = true                  -- Allow switching buffers without saving
 vim.o.tabstop = 4                    -- Set tab width to 4 spaces
 vim.o.shiftwidth = 4                 -- Set indentation width to 4 spaces
-vim.o.expandtab = false              -- Use spaces instead of tabs (false for now)
+vim.o.expandtab = false              -- Use spaces instead of tabs (disabled because norminette wouldn't like it)
 vim.o.termguicolors = true           -- Enable true color support
-
