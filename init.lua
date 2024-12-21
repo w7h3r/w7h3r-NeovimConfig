87b5567f825f5915a43c6ba653f5f88101b8e969
Arif OKCAN <142164597+w7h3r@users.noreply.github.com>
2024-12-21 11:08:27 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -2,4 +2,4 @@ require("config.lazy")
 require("settings.general")
 
 vim.g.mapleader = ' '
-vim.env.PATH = "/home/your~username/bin:" .. vim.env.PATH
+vim.env.PATH = "/home/your~username/bin:" .. vim.env.PATH --ripgrep path
