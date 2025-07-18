feca293be5071444fdd0c8ee3a2b2947378da4ee
Muhammet Arif Okcan <muokcan@k2m02s05.42kocaeli.com.tr>
2025-07-18 13:23:34 +0300

    Delete comment lines

--- lua/plugins/essential/copilot_chat.lua
+++ lua/plugins/essential/copilot_chat.lua
@@ -2,13 +2,11 @@ return {
   {
     "CopilotC-Nvim/CopilotChat.nvim",
     dependencies = {
-      { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
-      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+      { "github/copilot.vim" },
+      { "nvim-lua/plenary.nvim", branch = "master" },
     },
-    build = "make tiktoken", -- Only on MacOS or Linux
+    build = "make tiktoken",
     opts = {
-      -- See Configuration section for options
     },
-    -- See Commands section for default commands if you want to lazy load on them
   },
 }
