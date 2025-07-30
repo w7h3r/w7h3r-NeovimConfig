2e6d25c69cd2f0c589ecd5e12a078b70743af7a0
w7h3r <aarifokcan@gmail.com>
2025-07-30 21:11:02 +0300

    Add tab rule

--- init.lua
+++ init.lua
@@ -4,3 +4,18 @@ require("settings.general")
 vim.opt.termguicolors = true
 vim.g.mapleader = ' '
 vim.env.PATH = "/home/your~username/bin:" .. vim.env.PATH --ripgrep path
+vim.g.copilot_no_tab_map = 1
+
+vim.diagnostic.config({
+  virtual_text = {
+    prefix = '●',  -- veya '>>', '→' gibi bir şey de olabilir
+    source = true,
+  },
+  float = {
+    source = "always",
+    border = "rounded",
+  },
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+})
