bd51ee2c4205ea339c5dddadff6ce54b1f5f1ab3
w7h3r <aarifokcan@gmail.com>
2025-10-12 15:26:27 +0300

    Update lsp to Nvim x0.11+

--- lua/plugins/essential/lsp.lua
+++ lua/plugins/essential/lsp.lua
@@ -11,9 +11,12 @@ return {
     require('mason-lspconfig').setup({
       ensure_installed = {
         'clangd',
+        'lua_ls',
       },
     })
-    local lspconfig = require('lspconfig')
+
+    local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
     local on_attach = function(client, bufnr)
       require('lsp_signature').on_attach({
         bind = true,
@@ -23,12 +26,54 @@ return {
           border = 'rounded'
         }
       }, bufnr)
+
+      -- LSP key mappings
+      local bufopts = { noremap=true, silent=true, buffer=bufnr }
+      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+      vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+      vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
     end
-    lspconfig.clangd.setup({
+
+    vim.lsp.config.clangd = {
+      cmd = { 'clangd' },
+      filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda', 'proto' },
+      root_markers = { '.clangd', '.clang-tidy', '.clang-format', 'compile_commands.json', 'compile_flags.txt', 'configure.ac', '.git' },
+      capabilities = capabilities,
       on_attach = on_attach,
-      flags = {
-        debounce_text_changes = 150,
-      }
-    })
+      settings = {},
+    }
+
+    vim.lsp.config.lua_ls = {
+      cmd = { 'lua-language-server' },
+      filetypes = { 'lua' },
+      root_markers = { '.luarc.json', '.luarc.jsonc', '.luacheckrc', '.stylua.toml', 'stylua.toml', 'selene.toml', 'selene.yml', '.git' },
+      capabilities = capabilities,
+      on_attach = on_attach,
+      settings = {
+        Lua = {
+          runtime = {
+            version = 'LuaJIT'
+          },
+          diagnostics = {
+            globals = { 'vim' },
+          },
+          workspace = {
+            library = vim.api.nvim_get_runtime_file("", true),
+            checkThirdParty = false,
+          },
+          telemetry = {
+            enable = false,
+          },
+        },
+      },
+    }
+
+    vim.lsp.enable('clangd')
+    vim.lsp.enable('lua_ls')
   end
 }
