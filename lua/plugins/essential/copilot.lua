5f21f7310847162399c3bc389b0e91be3cc0cdca
w7h3r <aarifokcan@gmail.com>
2026-05-20 12:57:33 +0300

    Update Copilot config

--- lua/plugins/essential/copilot.lua
+++ lua/plugins/essential/copilot.lua
@@ -3,6 +3,7 @@ return {
   event = "InsertEnter",
   config = function()
     vim.g.copilot_no_tab_map = true
+	vim.g.copilot_enabled = false
     vim.keymap.set("i", "<C-l>", 'copilot#Accept("")', { expr = true, replace_keycodes = false })
   end,
 }
