bcd0ba0d0a53490a4c781cac5abaf9416e6dddc4
w7h3r <aarifokcan@gmail.com>
2026-06-09 17:37:43 +0300

    Diagnostics on editor has been minimized, because it looks pretty bad if you are making too many mistakes

--- init.lua
+++ init.lua
@@ -5,12 +5,16 @@ vim.opt.termguicolors = true
 vim.g.mapleader = ' '
 vim.env.PATH = "/home/your~username/bin:" .. vim.env.PATH --ripgrep path
 vim.g.copilot_no_tab_map = 1
+vim.keymap.set("n", "<leader>d", function()
+  vim.diagnostic.open_float()
+end)
 
 vim.diagnostic.config({
-  virtual_text = {
-    prefix = '●',
-    source = true,
-  },
+  virtual_text = false,
+  -- {
+  --   prefix = '●',
+  --   source = true,
+  -- },
   float = {
     source = "always",
     border = "rounded",
